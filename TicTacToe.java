9077d406c922eea3e70f1c8370ce917de8077a00
nirajkumar27297 <60932846+nirajkumar27297@users.noreply.github.com>
2020-08-04 23:25:00 +0530

    Adding Java Codes for UserCase 2

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,7 @@
-
+import java.util.*;
 public class TicTacToe {
 	private char[][] board = new char[3][3];
+	
 	
 	TicTacToe() {
 		for(int i = 0; i < 3; i++) {
@@ -9,11 +10,25 @@ public class TicTacToe {
 			}
 		}
 	}
+	public void play() {
+		//Creating random object
+		Random rand = new Random();
+		// Generating a random number in range[0,1] if 0 then computer chance and 1 then player chance
+		int checkOption = rand.nextInt(2);
+		
+		if(checkOption == 1) {
+			System.out.println("Player Chance to Play");
+		}
+		else {
+			System.out.println("Computer Chance to Play");
+		}
+	}
 
 	public static void main(String[] args) {
 		
 		//Creating objects for TicTacToe Game
 		TicTacToe gamePlay = new TicTacToe();
+		gamePlay.play();
 	}
 
 }
