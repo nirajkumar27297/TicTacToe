8231ed9aa4abe6a58db2fdf28a1e436e12bdca16
nirajkumar27297 <60932846+nirajkumar27297@users.noreply.github.com>
2020-08-04 23:46:49 +0530

    Adding Java codes for UserCase 4

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,8 +2,8 @@
 import java.util.*;
 public class TicTacToe {
 	private char[][] board= new char[3][3];
-	private char computerSymbol;
-	private char userSymbol;
+	private char userMove;
+	private char computerMove;
 	
 	TicTacToe() {
 		for(int i = 0; i < 3; i++) {
@@ -12,6 +12,14 @@ public class TicTacToe {
 			}
 		}
 	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Creating objects for TicTacToe Game
+		TicTacToe gamePlay = new TicTacToe();
+		gamePlay.play();
+	}
+	
 	public void play() {
 		//Creating Scanner class Object to take input 
 		Scanner sc = new Scanner(System.in);
@@ -20,42 +28,45 @@ public class TicTacToe {
 		char input;
 		// Generating a random number in range[0,1] if 0 then computer chance and 1 then player chance
 		int checkOption = rand.nextInt(2);
-		
+		this.displayBoard();
 		if(checkOption == 1) {
 			System.out.println("Player Chance to Play");
 			System.out.println("Choose Either X or O");
 			//Taking input from the user
 			input = sc.next().charAt(0);
-			this.userSymbol = input;
+			this.userMove = input;
 			if(input == 'X') {
-				this.computerSymbol = 'O';
+				this.computerMove = 'O';
 			}
 			else {
-				this.computerSymbol = 'X';
-			}
+				this.computerMove = 'X';
+			}	
 		}
-		
 		else {
 			System.out.println("Computer Chance to Play");
 			//Generating a random number in range[0,1] if 0 then input is "O" else "X"
 			int computerChance = rand.nextInt(2);
 			if(computerChance == 0) {
-				this.computerSymbol = 'O';
-				this.userSymbol = 'X';
+				this.computerMove = 'O';
+				this.userMove = 'X';
 			}
 			else {
-				this.computerSymbol = 'X';
-				this.userSymbol = 'O';
+				this.computerMove = 'X';
+				this.userMove = 'O';
 			}
-			System.out.println("The computer chooses "+this.computerSymbol);
+			System.out.println("The computer chooses "+this.computerMove);
 		}
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Creating objects for TicTacToe Game
-		TicTacToe gamePlay = new TicTacToe();
-		gamePlay.play();
+	
+	public void displayBoard() {
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				System.out.print("|");
+				System.out.print(this.board[i][j]+" ");
+				
+			}
+			System.out.println();
+			System.out.println("---------");
+		}
 	}
-
 }
