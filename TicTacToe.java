ec312211fb92203344ccb993dbf72fc0ad78583e
nirajkumar27297 <60932846+nirajkumar27297@users.noreply.github.com>
2020-08-05 20:06:49 +0530

    Refracting Java Codes for UserCase 5

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,23 +1,27 @@
 
 import java.util.*;
 public class TicTacToe {
-	private char[][] board= new char[3][3];
+	private char[][] board;
 	private char userSymbol;
 	private char computerSymbol;
-	
+	private ArrayList<Integer> playerPosition;
+	private ArrayList<Integer> computerPosition;
 	TicTacToe() {
+		board = new char[3][3];
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
 				this.board[i][j] = ' ';
 			}
 		}
-	}
+		playerPosition = new ArrayList<Integer>();
+		computerPosition = new ArrayList<Integer>();
+		}
 	public static void main(String[] args) {
+		
 		//Creating objects for TicTacToe Game
 		TicTacToe gamePlay = new TicTacToe();
 		gamePlay.play();
 	}
-	
 	public void play() {
 		//Creating Scanner class Object to take input 
 		Scanner sc = new Scanner(System.in);
@@ -28,18 +32,28 @@ public class TicTacToe {
 		// Generating a random number in range[0,1] if 0 then computer chance and 1 then player chance
 		int checkOption = rand.nextInt(2);
 		this.displayBoard();
+		
 		if(checkOption == 1) {
-			System.out.println("Player Chance to Play");
-			System.out.println("Choose Either X or O");
-			//Taking input from the user
-			input = sc.next().charAt(0);
-			this.userSymbol = input;
-			System.out.println("You chose "+this.userSymbol);
-			if(input == 'X') {
-				this.computerSymbol = 'O';
-			}
-			else {
-				this.computerSymbol = 'X';
+			while(true) {
+				System.out.println("Player Chance to Play");
+				System.out.println("Choose Either X or O");
+				//Taking input from the user
+				input = sc.next().charAt(0);
+				if(input != 'X') {
+					System.out.println("Enter Right Value");
+					continue;
+				}
+				this.userSymbol = input;
+				System.out.println("You chose "+this.userSymbol);
+				
+				if(input == 'X') {
+					
+					this.computerSymbol = 'O';
+				}
+				else {
+					this.computerSymbol = 'X';
+				}
+				break;
 			}	
 		}
 		
@@ -58,85 +72,101 @@ public class TicTacToe {
 			System.out.println("The computer chooses "+this.computerSymbol);
 			
 		}
+		//start playing
 		this.startPlaying(checkOption);
-	
 	}
+	//Game started with turn
 	public void startPlaying(int turn) {
 		Scanner sc = new Scanner(System.in);
 		int move;
-		char symbol;
 		while(true) {
-					
-		
-		if(turn == 1) {
-			symbol = this.userSymbol;
-			System.out.println("Enter the move in range[1-9]");
-			move = sc.nextInt();
-			move -= 1;
-			boolean moveValid = this.moveValid(move);
-			
-			if(!moveValid ) {
-				System.out.println("Invalid Move Try Once More");
-				continue;			
-			}	
-		}
-		else {
-			symbol = this.computerSymbol;
-			move = this.computerMove();
-			boolean moveValid = this.moveValid(move);
-			
-			if(!moveValid ) {
-				System.out.println("Invalid Move Try Once More");
-				continue;
-			}
-		}
-		this.board[(int)(move / 3)][move % 3] = symbol;
-		System.out.println(2);
-		this.displayBoard();
-		
-		String resultWinner=this.checkWinner();
-		
-		if(resultWinner != null ) {
-			System.out.println(resultWinner);
-			if(resultWinner.charAt(0) == this.computerSymbol) {
-				System.out.println("The winner is Computer");
+			if(turn == 1) {
+				System.out.println("Enter the move in range[1-9]");
+				move = sc.nextInt();
+				move -= 1;
+				boolean moveValid = moveValid(move);
 				
-			}
-			else if(resultWinner.charAt(0) == this.userSymbol) {
-				System.out.println("The winner is Player");
-				
+				if(!moveValid ) {
+					System.out.println("Invalid Move Try Once More");
+					continue;			
+				}
+				this.playerPosition.add(move);
+				this.board[(int)(move / 3)][move % 3] = this.userSymbol;
 			}
 			else {
-				System.out.println("The Game is Draw");
+				System.out.println("Computer Turn");
+				computerMove();
 			}
 			
 			this.displayBoard();
-			return;
-		}
+			
+			String resultWinner=this.checkWinner();
+			boolean dre = this.draw();
+			if(resultWinner != null ) {
+				System.out.println(resultWinner);
+				if(resultWinner.charAt(0) == this.computerSymbol) {
+					System.out.println("The winner is Computer");
+					
+				}
+				else if(resultWinner.charAt(0) == this.userSymbol) {
+					System.out.println("The winner is Player");
+					
+				}
+				else {
+					System.out.println("The Game is Draw");
+				}
+				
+				this.displayBoard();
+				return;
+			}
 		
-		if(turn == 0) {
-			turn = 1;
-		}
-		else {
-			turn = 0;
-		}
-		
+			if(turn == 0) {
+				turn = 1;
+			}
+			else {
+				turn = 0;
+			}	
 	}
 	}
 	
-	public int computerMove() {
-		Random rand = new Random();
-		int move = rand.nextInt(9);
-		return move;
-	}
-		
+	public void computerMove() {																			
+		//Random Sides
+		while(true) {
+			Random rand = new Random();
+			int move = rand.nextInt(9);
+			if(this.moveValid(move)) {
+				this.board[(int)(move / 3)][move % 3] = this.computerSymbol;
+				this.computerPosition.add(move);
+				return;
+			}	
+		}
+		}	
 	
-	boolean equals3(char c1, char c2, char c3) {
-		  return (c1 == c2 && c2 == c3 && c1 != ' ');
+	public boolean moveValid(int move) {
+		if(this.board[(int)(move / 3)][move % 3] == ' ') {
+			return true;
+		}
+		return false;	
+		}
+	
+	public void displayBoard() {
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.print("|");
+				System.out.print(this.board[i][j]+" ");
+				
+			}
+			System.out.println();
+			System.out.println("---------");
+		}
 	}
+	boolean equals3(char a, char b, char c) {
+		  return (a == b && b == c && a != ' ');
+		}
 	
 	public String checkWinner() {
 	  String winner = null;
+
 	  // horizontal
 	  for (int i = 0; i < 3; i++) {
 	    if (this.equals3(board[i][0], board[i][1], board[i][2])) {
@@ -160,41 +190,22 @@ public class TicTacToe {
 	  }
 	  
 	  
-	  if (this.staleMate() == true && winner == null  ) {
+	  if (this.draw() == true && winner == null  ) {
 		  return "tie";
 	  } else {
 	    return winner;
 	  }
 	}
-	//Checking for staleMate
-	public boolean staleMate() {
+	
+	//Checking for draw
+	public boolean draw() {
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
-				if (this.board[i][j] != ' ') {
+				if (this.board[i][j] == ' ') {
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-	
-	public void displayBoard() {
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				System.out.print("|");
-				System.out.print(this.board[i][j]+" ");	
-			}
-			System.out.println();
-			System.out.println("---------");
-			}	
-		}
-	
-	public boolean moveValid(int move) {
-		if(this.board[(int)(move / 3)][move % 3] == ' ') {
-			return true;
-		}
-		return false;
-			
-		}
 }
-
